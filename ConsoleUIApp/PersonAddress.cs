8acbd7c61fb750235df71c5ed1c0fae67744a2dd
Munyangabo <munyangabo@hk.local>
2022-10-11 11:55:04 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleUIApp/PersonAddress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUIApp
+{
+    public class PersonAddress
+    {
+
+        public string Street { private get; set; }
+        //public string Address { get; set; }      
+
+       
+
+        public string City { private get; set; }
+        public string State {  private get; set; }
+        public int Zipcode { private get; set; }
+
+        public string FullAddress
+        {
+            get
+
+            {
+                return $" My address is : {Street} {Zipcode },{City} {State}";
+
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleUIApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleUIApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonAddress address = new PersonAddress();
+            address.City = "Hannover";
+            address.State = "Niedersaschen";
+            address.Street = " Kopernikustraßen 7";
+            address.Zipcode = 300321;
+            
+
+            Console.WriteLine(address.FullAddress);
+            
+
+        }
+    }
+}
